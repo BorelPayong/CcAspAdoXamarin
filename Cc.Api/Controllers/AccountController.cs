b2b6d0b16f3d49f8977e007e577761e6ff8454b7
bfranck163@gmail.com <bfranck163@gmail.com>
2021-04-13 12:41:19 +0200

    Ajout controleur pour liste proprio et bien

---
+++ Cc.Api/Controllers/AccountController.cs
@@ -0,0 +1,62 @@
+﻿using Cc.Api.Entities;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity.Infrastructure;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Web;
+using System.Web.Http;
+using System.Web.Mvc;
+using HttpGetAttribute = System.Web.Http.HttpGetAttribute;
+
+namespace Cc.Api.Controllers
+{
+    public class AccountController : Controller
+    {
+        private CcAspAdoXamarinDbEntities db = new CcAspAdoXamarinDbEntities();
+
+        [HttpGet]
+        public IHttpActionResult ListeProprietaire(int index = 0, int taille = 10)
+        {
+            try
+            {
+
+                var proprietaires = db.Proprietaire
+                    .OrderByDescending(x => x.DateDeCreation)
+                    .Skip(index * taille).Take(taille).ToList();
+                return Ok(proprietaires);
+            }
+            catch (DbUpdateException ex)
+            {
+                var exception = ex.InnerException?.InnerException as SqlException;
+                return BadRequest(exception?.Message);
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(ex.Message);
+            }
+        }
+
+        [HttpGet]
+        public IHttpActionResult ListeBien(int index = 0, int taille = 10)
+        {
+            try
+            {
+
+                var biens = db.Proprietaire
+                    .OrderByDescending(x => x.DateDeCreation)
+                    .Skip(index * taille).Take(taille).ToList();
+                return Ok(biens);
+            }
+            catch (DbUpdateException ex)
+            {
+                var exception = ex.InnerException?.InnerException as SqlException;
+                return BadRequest(exception?.Message);
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(ex.Message);
+            }
+        }
+    }
+}
